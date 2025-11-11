e35fcae4ab518608578df72bb1ee041afab84434
Creepybits <zanno.jacklin@hotmail.com>
2025-11-11 16:41:46 +0100

    Update MasterKey.py

--- assets/nodes/MasterKey.py
+++ assets/nodes/MasterKey.py
@@ -4,7 +4,7 @@ import comfy.sd
 import comfy.utils
 import re
 
-class MasterKey:
+class WW_MasterKey:
 
     def __init__(self):
         script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -35,7 +35,7 @@ class MasterKey:
 
     FUNCTION = "concat_texts"
 
-    CATEGORY = "Creepybits/Prompt"
+    CATEGORY = "Creepybits/World_weaver"
 
     def concat_texts(self, text_2):
         combined_text = self.fixed_text + text_2
@@ -43,9 +43,10 @@ class MasterKey:
 
 
 NODE_CLASS_MAPPINGS = {
-      "MasterKey": MasterKey,
+      "WW_MasterKey": WW_MasterKey,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-      "MasterKey": "Master Key (Creepybits)",
+      "WW_MasterKey": "WW Master Key (Creepybits)",
 }
+
