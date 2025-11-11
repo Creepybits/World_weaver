d0177b7b4fa6283fc288a626637ae4b449e7d771
Creepybits <zanno.jacklin@hotmail.com>
2025-11-11 16:47:29 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,15 +9,15 @@ assets_nodes_path = os.path.join(custom_nodes_path, "assets", "nodes")
 sys.path.append(assets_nodes_path)
 
 # --- IMPORT ALL YOUR NODES FROM assets/nodes ---
-from .assets.nodes.DelayTextNode import NODE_CLASS_MAPPINGS as DelayTextNode_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as DelayTextNode_NODE_DISPLAY_NAMES
-from .assets.nodes.GeminiAPI import NODE_CLASS_MAPPINGS as GeminiAPI_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as GeminiAPI_NODE_DISPLAY_NAMES
-from .assets.nodes.conditional_lora_selector import NODE_CLASS_MAPPINGS as ConditionalLoRAApplier_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as ConditionalLoRAApplier_NODE_DISPLAY_NAMES
-from .assets.nodes.MasterKey import NODE_CLASS_MAPPINGS as MasterKey_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as MasterKey_NODE_DISPLAY_NAMES
-from .assets.nodes.SceneDirector import NODE_CLASS_MAPPINGS as SceneDirector_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as SceneDirector_NODE_DISPLAY_NAMES
-from .assets.nodes.QwenAspectRatio import NODE_CLASS_MAPPINGS as QwenAspectRatio_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as QwenAspectRatio_NODE_DISPLAY_NAMES
-from .assets.nodes.CharacterVault import NODE_CLASS_MAPPINGS as CharacterVault_NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as CharacterVault_NODE_DISPLAY_NAME_MAPPINGS
-from .assets.nodes.CharacterSelect import NODE_CLASS_MAPPINGS as CharacterSelect_NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as CharacterSelect_NODE_DISPLAY_NAME_MAPPINGS
-from .assets.nodes.WorldWeaverPrompt import NODE_CLASS_MAPPINGS as WorldWeaverPrompt_NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as WorldWeaverPrompt_NODE_DISPLAY_NAME_MAPPINGS
+from .assets.nodes.DelayTextNode import NODE_CLASS_MAPPINGS as WW_DelayTextNode_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as WW_DelayTextNode_NODE_DISPLAY_NAMES
+from .assets.nodes.GeminiAPI import NODE_CLASS_MAPPINGS as WW_GeminiAPI_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as WW_GeminiAPI_NODE_DISPLAY_NAMES
+from .assets.nodes.conditional_lora_selector import NODE_CLASS_MAPPINGS as WW_ConditionalLoRAApplier_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as WW_ConditionalLoRAApplier_NODE_DISPLAY_NAMES
+from .assets.nodes.MasterKey import NODE_CLASS_MAPPINGS as WW_MasterKey_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as WW_MasterKey_NODE_DISPLAY_NAMES
+from .assets.nodes.SceneDirector import NODE_CLASS_MAPPINGS as WW_SceneDirector_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as WW_SceneDirector_NODE_DISPLAY_NAMES
+from .assets.nodes.QwenAspectRatio import NODE_CLASS_MAPPINGS as WW_QwenAspectRatio_NODE_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as WW_QwenAspectRatio_NODE_DISPLAY_NAMES
+from .assets.nodes.CharacterVault import NODE_CLASS_MAPPINGS as WW_CharacterVault_NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as WW_CharacterVault_NODE_DISPLAY_NAME_MAPPINGS
+from .assets.nodes.CharacterSelect import NODE_CLASS_MAPPINGS as WW_CharacterSelect_NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as WW_CharacterSelect_NODE_DISPLAY_NAME_MAPPINGS
+from .assets.nodes.WorldWeaverPrompt import NODE_CLASS_MAPPINGS as WW_WorldWeaverPrompt_NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as WW_WorldWeaverPrompt_NODE_DISPLAY_NAME_MAPPINGS
 
 
 
@@ -36,31 +36,31 @@ except ImportError:
 
 # --- CONSOLIDATE ALL NODE MAPPINGS ---
 NODE_CLASS_MAPPINGS = {
-    **DelayTextNode_NODE_MAPPINGS,    
-    **GeminiAPI_NODE_MAPPINGS,    
-    **ConditionalLoRAApplier_NODE_MAPPINGS,    
-    **MasterKey_NODE_MAPPINGS,    
-    **SceneDirector_NODE_MAPPINGS,    
-    **QwenAspectRatio_NODE_MAPPINGS,   
-    **CharacterVault_NODE_CLASS_MAPPINGS,
-    **CharacterSelect_NODE_CLASS_MAPPINGS,
-    **WorldWeaverPrompt_NODE_CLASS_MAPPINGS,
+    **WW_DelayTextNode_NODE_MAPPINGS,    
+    **WW_GeminiAPI_NODE_MAPPINGS,    
+    **WW_ConditionalLoRAApplier_NODE_MAPPINGS,    
+    **WW_MasterKey_NODE_MAPPINGS,    
+    **WW_SceneDirector_NODE_MAPPINGS,    
+    **WW_QwenAspectRatio_NODE_MAPPINGS,   
+    **WW_CharacterVault_NODE_CLASS_MAPPINGS,
+    **WW_CharacterSelect_NODE_CLASS_MAPPINGS,
+    **WW_WorldWeaverPrompt_NODE_CLASS_MAPPINGS,
 
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = { 
-    **DelayTextNode_NODE_DISPLAY_NAMES,   
-    **GeminiAPI_NODE_DISPLAY_NAMES,   
-    **ConditionalLoRAApplier_NODE_DISPLAY_NAMES,    
-    **MasterKey_NODE_DISPLAY_NAMES,   
-    **SceneDirector_NODE_DISPLAY_NAMES,   
-    **QwenAspectRatio_NODE_DISPLAY_NAMES,   
-    **CharacterVault_NODE_DISPLAY_NAME_MAPPINGS,
-    **CharacterSelect_NODE_DISPLAY_NAME_MAPPINGS,
-    **WorldWeaverPrompt_NODE_DISPLAY_NAME_MAPPINGS,
+    **WW_DelayTextNode_NODE_DISPLAY_NAMES,   
+    **WW_GeminiAPI_NODE_DISPLAY_NAMES,   
+    **WW_ConditionalLoRAApplier_NODE_DISPLAY_NAMES,    
+    **WW_MasterKey_NODE_DISPLAY_NAMES,   
+    **WW_SceneDirector_NODE_DISPLAY_NAMES,   
+    **WW_QwenAspectRatio_NODE_DISPLAY_NAMES,   
+    **WW_CharacterVault_NODE_DISPLAY_NAME_MAPPINGS,
+    **WW_CharacterSelect_NODE_DISPLAY_NAME_MAPPINGS,
+    **WW_WorldWeaverPrompt_NODE_DISPLAY_NAME_MAPPINGS,
 }
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 WEB_DIRECTORY = "./web"
@@ -75,6 +75,7 @@ for node_name in NODE_CLASS_MAPPINGS.keys():
 if 'WEB_DIRECTORY' in locals():
     print(f"Web directory for custom UI: {WEB_DIRECTORY}")
 print("-------------------------------------------------------------------")
+
 
 
 
